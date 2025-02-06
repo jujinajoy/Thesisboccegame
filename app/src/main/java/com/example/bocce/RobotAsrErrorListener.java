53070dc8447fae14f22d9169fa03886e4820bad6
Jujina Joy <jujina.joy@tu-clausthal.de>
2025-02-06 15:17:09 +0100

    Adding Python and Android

--- app/src/main/java/com/example/bocce/RobotAsrErrorListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.bocce;
-import org.jetbrains.annotations.NotNull;
-
-
-public interface RobotAsrErrorListener {
-    void onAsrError(int errorCode, @NotNull String errorMessage);
-}
